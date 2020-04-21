fefa18b523443cbd422040e2c8087870ed3a9683
Rudraneel <callofdutygameofpc@gmail.com>
2020-04-21 17:53:33 +0530

    My car is in world war

--- sketch.js
+++ sketch.js
@@ -1,47 +1,27 @@
-var ball,database;
-var position;
+
+
+
+
+
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+
+var database;
+
+var form, player, game;
+
 
 function setup(){
-    database=firebase.database();
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
-    var ballPosition=database.ref('Ball/positions');
-    ballPosition.on("value",readPosition,showError);
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
+
 
 function draw(){
-  
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
 }
-
-
-
-function writePosition(x,y){
-    database.ref('Ball/positions').set({
-    'x':position.x + x,
-    'y':position.y + y
-})
-}
-function readPosition(data){
-    position=data.val();
-    console.log(position.x);
-    ball.x=position.x;
-    ball.y=position.y;
-}
-function showError (){
-    console.log("error");
-}
+//Let's fight corona
